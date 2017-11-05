8602eaa2c3e85ceb86e10837d67c97e55c5e4a92
Aiden <aidenchiavatti@gmail.com>
2017-11-05 04:45:29 -0500

    Stock prices chart functionality added

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.get('/homepage.html', function (req, res) {
     res.sendfile('homepage.html')
 })
 
+app.get("/historicals", function(req, res) {
+  console.log("/historicals called with " + req.query.symbol)
+  getHistoricalStocks(req.query.symbol, (hist)=> {res.send(hist)})
+})
+
 
 
 //Twitter timeline api 
@@ -86,6 +91,67 @@ personality_insights.profile({
       //console.log(JSON.stringify(response, null, 2));
       printFile(JSON.stringify(response, null, 2));
 });
+
+
+//Robinhood API
+var credentials = {
+    username: 'aidenchiavatti@gmail.com',
+    password: 'aiden123red'
+};
+
+function getHistoricalStocks(stockSymbol, callback) {
+  console.log("getting stocks")
+  var historicals = [];
+  var Robinhood = require('robinhood')(credentials, function(){
+      //{interval=5minute|10minute (required) span=week|day| }
+   
+      Robinhood.historicals(stockSymbol, '10minute', 'day', function(err, response, body){
+          if(err){
+              console.error(err);
+          }else{
+              console.log("got historicals");
+              for(i = 0; i < body["historicals"].length; i++) {
+                  historicals.push(body["historicals"][i].open_price)
+              }
+              console.log(JSON.stringify(historicals));
+              callback(historicals)
+              //return historicals
+              //callback()
+              //             
+              //    { quote: 'https://api.robinhood.com/quotes/AAPL/',
+              //      symbol: 'AAPL',
+              //      interval: '5minute',
+              //      span: 'week',
+              //      bounds: 'regular',
+              //      previous_close: null,
+              //      historicals:
+              //       [ { begins_at: '2016-09-15T13:30:00Z',
+              //           open_price: '113.8300',
+              //           close_price: '114.1700',
+              //           high_price: '114.3500',
+              //           low_price: '113.5600',
+              //           volume: 3828122,
+              //           session: 'reg',
+              //           interpolated: false },
+              //         { begins_at: '2016-09-15T13:35:00Z',
+              //           open_price: '114.1600',
+              //           close_price: '114.3800',
+              //           high_price: '114.7300',
+              //           low_price: '114.1600',
+              //           volume: 2166098,
+              //           session: 'reg',
+              //           interpolated: false },
+              //         ... 290 more items
+              //      ]}
+              //
+          }
+      })
+  })
+  //return callback
+}
+
+
+
 
 
 
